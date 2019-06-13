ce160ee2143c48cb28b9412ce0ef97a675b169a8
aryltsov <aryltsov@ukr.net>
2019-06-13 11:49:16 +0300

    удалены лишнии элементы некоторых компонентов

--- back/app.js
+++ back/app.js
@@ -5,11 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 
-var indexRouter      = require('./routes/index');
-var usersRouter      = require('./routes/users');
-var mailsRouter      = require('./routes/mails');
-var loginRouter      = require('./routes/login');
-var verifiedRouter   = require('./routes/verified_email');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var mailsRouter = require('./routes/mails');
+var loginRouter = require('./routes/login');
+var verifiedRouter = require('./routes/verified_email');
 var unverifiedRouter = require('./routes/unverified');
 var testRouter = require('./routes/test');
 
@@ -21,13 +21,13 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -38,27 +38,26 @@ app.use('/get_mails', mailsRouter);
 app.use('/send_verified', verifiedRouter);
 app.use('/send_unverified', unverifiedRouter);
 
-app.use('/test',testRouter);
+app.use('/test', testRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
-
-app.post('/', function(req, res) {
-  res.send('hello world');
+app.post('/', function (req, res) {
+    res.send('hello world');
 });
 
 
--- front/src/app/pages/verify-mail/verify-mail.component.ts
+++ front/src/app/pages/verify-mail/verify-mail.component.ts
@@ -1,55 +1,53 @@
 import {Component, Input, OnInit} from '@angular/core';
-import { UnverifiedService} from '../../providers/unverified.service';
-import { NbToastrService } from '@nebular/theme';
-
+import {UnverifiedService} from '../../providers/unverified.service';
+import {NbToastrService} from '@nebular/theme';
 
 
 @Component({
-  selector: 'ngx-verify-mail',
-  templateUrl: './verify-mail.component.html',
-  styleUrls: ['./verify-mail.component.scss']
+    selector: 'ngx-verify-mail',
+    templateUrl: './verify-mail.component.html',
+    styleUrls: ['./verify-mail.component.scss']
 })
 export class VerifyMailComponent implements OnInit {
 
-  userEmail: string;
+    userEmail: string;
 
-  constructor(public unverifiedService: UnverifiedService, private toastrService: NbToastrService) { }
+    constructor(public unverifiedService: UnverifiedService, private toastrService: NbToastrService) {
+    }
 
-  sendUnverifiedEmail(value) {
+    sendUnverifiedEmail(value) {
 
-      this.unverifiedService.sendUnverifiedMail(value).subscribe((res) => {
-        this.userEmail = res;
-      });
+        this.unverifiedService.sendUnverifiedMail(value).subscribe((res) => {
+            this.userEmail = res;
+        });
 
-      const message = 'The message has been sent';
+        const message = 'The message has been sent';
+        this.showToast('top-left', 'success', message, '');
+    }
 
-      this.showToast('top-left', 'success', message, '');
-  }
-  sendVerifiedEmail(value) {
+    sendVerifiedEmail(value) {
 
-    this.unverifiedService.sendVerifiedMail(value).subscribe((res) => {
-      this.userEmail = res;
-    });
+        this.unverifiedService.sendVerifiedMail(value).subscribe((res) => {
+            this.userEmail = res;
+        });
 
-    const message = 'The message has been sent';
+        const message = 'The message has been sent';
+        this.showToast('top-left', 'success', message, '');
+    }
 
-    this.showToast('top-left', 'success', message, '');
+    ngOnInit() {
+    }
 
-  }
-
-  ngOnInit() {
-  }
-
-  showToast(position, status, mess, title) {
-    this.toastrService.show(
-      mess,
-      title,
-      {
-        duration: 5000,
-        position: position,
-        status: status
-      }
-    );
-  }
+    showToast(position, status, mess, title) {
+        this.toastrService.show(
+            mess,
+            title,
+            {
+                duration: 5000,
+                position: position,
+                status: status
+            }
+        );
+    }
 
 }
